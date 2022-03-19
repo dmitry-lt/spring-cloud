78baa2c3d268c4614dde10c93d6d8852e18bfc96
Dmitry <dmitry-lt@users.noreply.github.com>
2022-03-19 20:00:18 +0300

    dog breed service

---
+++ dog-breed-service/src/main/java/demo/sc/dogbreed/DogBreedServiceApplication.java
@@ -0,0 +1,13 @@
+package demo.sc.dogbreed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogBreedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogBreedServiceApplication.class, args);
+	}
+
+}
