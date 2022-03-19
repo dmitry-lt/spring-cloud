bd9d60bb38a87892334fdbc71b3087d1fceb6d61
Dmitry <dmitry-lt@users.noreply.github.com>
2022-03-19 20:47:47 +0300

    dog breeds

---
+++ dog-breed-service/src/main/java/demo/sc/dogbreed/service/DogBreedService.java
@@ -0,0 +1,11 @@
+package demo.sc.dogbreed.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DogBreedService {
+    public String getRandomBreed() {
+        // TODO: implement
+        return "ENGLISH SETTER";
+    }
+}
